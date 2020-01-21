3164ce949e938ec0928f47f013e89c9166ae2c64
Khalil Stemmler <metroidman12@gmail.com>
2020-01-21 17:26:11 -0500

    Need LAUNCH_TILE_DATA to be defined initially in order to start the app without errors.

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,5 +1,22 @@
 import React, { Fragment }  from 'react';
+import { gql } from '@apollo/client'
 import { RouteComponentProps } from '@reach/router';
+
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    __typename
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
 
 interface LaunchesProps extends RouteComponentProps {}
 
